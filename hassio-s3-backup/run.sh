59b4192ec91b7a3f8792244e4f4935dd25a4f3a2
prankstr <11200347+prankstr@users.noreply.github.com>
2024-07-08 14:33:12 +0200

    Remove config bloat

--- hassio-s3-backup/run.sh
+++ hassio-s3-backup/run.sh
@@ -5,17 +5,11 @@ CONFIG_PATH=/data/options.json
 bashio::log.info "Setting up configuration"
 
 # Create main config
-export STORAGE_BACKEND=$(bashio::config 'storage_backend')
 export LOG_LEVEL=$(bashio::config 'log_level')
-export BACKUP_DIRECTORY=$(bashio::config 'backup_directory')
-export PROTON_DRIVE_USERNAME=$(bashio::config 'proton.username')
-export PROTON_DRIVE_PASSWORD=$(bashio::config 'proton.password')
-export STORJ_BUCKET_NAME=$(bashio::config 'storj.bucket')
-export STORJ_ACCESS_GRANT=$(bashio::config 'storj.access_grant')
-export S3_ENDPOINT=$(bashio::config 's3.endpoint')
-export S3_BUCKET_NAME=$(bashio::config 's3.bucket')
-export S3_ACCESS_KEY_ID=$(bashio::config 's3.access_key_id')
-export S3_SECRET_ACCESS_KEY=$(bashio::config 's3.secret_access_key')
+export S3_ENDPOINT=$(bashio::config 's3_endpoint')
+export S3_BUCKET_NAME=$(bashio::config 's3_bucket')
+export S3_ACCESS_KEY_ID=$(bashio::config 's3_access_key_id')
+export S3_SECRET_ACCESS_KEY=$(bashio::config 's3_secret_access_key')
 
 # Start application
 bashio::log.info "Starting Home Assistant Proton Drive Backup"
