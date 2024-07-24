e5fedf4b3e52132811c6bfbe0b8b0117e843ec7b
prankstr <11200347+prankstr@users.noreply.github.com>
2024-07-24 09:51:46 +0200

    Test

--- hassio-s3-backup/internal/hassio/hassio.go
+++ hassio-s3-backup/internal/hassio/hassio.go
@@ -21,18 +21,11 @@ type Addon struct {
 
 // Backup represents the details of a backup in Home Assistant
 type Backup struct {
-	Date              time.Time `json:"date"`
-	Location          *string   `json:"location"`
-	HomeAssistant     string    `json:"homeassistant"`
-	Slug              string    `json:"slug"`
-	Name              string    `json:"name"`
-	Type              string    `json:"type"`
-	SupervisorVersion string    `json:"supervisor_version"`
-	Repositories      []string  `json:"repositories"`
-	Addons            []Addon   `json:"addons"`
-	Folders           []string  `json:"folders"`
-	Size              float64   `json:"size"`
-	Protected         bool      `json:"protected"`
+	Date time.Time `json:"date"`
+	Slug string    `json:"slug"`
+	Name string    `json:"name"`
+	Type string    `json:"type"`
+	Size float64   `json:"size"`
 }
 
 // BackupResponse represents the response from Home Assistant for listing backups
